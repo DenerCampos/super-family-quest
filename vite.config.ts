6e70c38b42e113ba9ec7d3d12c9589e64cec097a
DenerCampos <denerjcampos@gmail.com>
2026-03-26 16:04:05 -0300

    fix(asset-paths): update asset paths in index.html and vite.config.ts to include 'public' directory

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['super-family-quest.png'],
+      includeAssets: ['public/super-family-quest.png'],
       manifest: {
         name: 'Super Family Quest',
         short_name: 'Family Quest',
@@ -20,12 +20,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/icons/icon-192x192.png',
+            src: '/public/icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/icons/icon-512x512.png',
+            src: '/public/icons/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
